601ebd24754eab38fdfe79f499d62777c13a4739
Emma <emma.follette@gmail.com>
2016-12-17 20:47:50 -0800

    [Refactored] Returns bearer token in console

--- app.js
+++ app.js
@@ -3,27 +3,53 @@
 var http = require('http');
 var HttpDispatcher = require('httpdispatcher');
 var OAuth2 = require('oauth').OAuth2;
+var queryString = require('querystring');
+var util = require('util');
 
-var dispatcher = new HttpDispatcher();
+const PORT = 3000;
+const CONSUMERKEY = '';
+const CONSUMERSECRET = '';
 
-const PORT=3000;
+var redirectURI = 'http://localhost:3000/auth/linkedin/callback';
+var baseSite = 'https://www.linkedin.com/oauth/v2';
+var authorizePath = '/authorization';
+var accessTokenPath = '/accessToken';
 
-function handleRequest(request, response){
-  try{
-    console.log(request.url);
-    dispatcher.dispatch(request, response);
-  } catch(err){
-    console.log(err);
-  }
-}
+var oauth2 = new OAuth2(CONSUMERKEY, CONSUMERSECRET, baseSite, authorizePath, accessTokenPath, null);
 
-var server = http.createServer(handleRequest);
+var server = http.createServer(function(request, response){
+  var path = request.url.split('/');
 
-dispatcher.onGet('/auth/linkedin/callback', function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Success!');
-});
+  var authURL = oauth2.getAuthorizeUrl({
+    response_type: 'code',
+    redirect_uri: redirectURI,
+    scope: ['r_basicprofile rw_ad_campaigns'],
+    state: 'some string for CSRF prevention'
+  });
 
-server.listen(PORT, function(){
-  console.log('Example app listening to port %s', PORT)
-});
+  var body = '<a href="' + authURL + '">Click here get authorization code</a>';
+  response.writeHead(200, {
+    'Content-Type':'text/html'
+  });
+  response.end(body);
+
+  var queryStringObj = {};
+  if (path[3] != null && path.length !== 0 && path != 'undefined') {
+    queryStringObj = queryString.parse(path[3].split('?')[1]);
+
+    oauth2.getOAuthAccessToken(queryStringObj.code, {
+      grant_type: 'authorization_code',
+      redirect_uri: redirectURI
+    }, function(error, access_token, results){
+      if (error){
+        console.log(error);
+        response.end(error);
+      }
+      else {
+        console.log('Obtained access_token', access_token);
+        response.end(access_token);
+      }
+    });
+  };
+
+}).listen(3000);
--- test/differ.js
+++ test/differ.js
@@ -2,6 +2,30 @@
 
 var assert = require('assert');
 
+describe('OAuth 2.0 Authorization',function(){
+  var OAuth = require('oauth');
+
+   it('should get bearer token for LinkedIn member', function(done){
+     var OAuth2 = OAuth.OAuth2;
+     var CONSUMERKEY = '';
+     var CONSUMERSECRET = '';
+     var oauth2 = new OAuth2(
+       CONSUMERKEY,
+       CONSUMERSECRET,
+       'https://www.linkedin.com/oauth/v2/',
+       null,
+       'oauth2/token',
+       null);
+     oauth2.getOAuthAccessToken(
+       '',
+       {'grant_type':'client_credentials'},
+       function (e, access_token, refresh_token, results){
+       console.log('bearer: ',access_token);
+       done();
+     });
+   });
+});
+
 describe('API v1 resource', function(){
   it('should return the results from v1 API');
 });
